3a1e29fe41e82795ebd2bd04678ea3c1916e4a2a
Johnny Murillo <jossters@gmail.com>
2021-11-04 04:14:08 -0700

    changed image and txt

---
+++ client/src/components/Home.js
@@ -0,0 +1,21 @@
+import burger from '../burger.png'
+import '../App.css';
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div className="App">
+      <header className="App-header">
+        <img src={burger} className="App-logo" alt="logo" />
+        <p>
+          
+        </p>
+        <p>
+          Simplujj Burgers
+        </p>
+      </header>
+    </div>
+    );
+}
+
+export default Home;
