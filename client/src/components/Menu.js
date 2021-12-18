f4b0b848541bea2ee99095f7df446b66d6c19502
Johnny Murillo <jossters@gmail.com>
2021-12-17 20:23:36 -0800

    tab menu

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,60 +1,87 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 import "../App.css";
 import { burgers, drinks, sides, tabs } from "../data";
 
+const Tab = styled.button`
+  height: 40px;
+  width: 100px;
+  padding: 10px 30px;
+  cursor: pointer;
+  opacity: 0.6;
+  background: white;
+  border: 0;
+  outline: 0;
+  border-bottom: 2px solid transparent;
+  transition: ease border-bottom 250ms;
+  ${({ active }) =>
+    active &&
+    `
+    border-bottom: 2px solid blue;
+    opacity: 1;
+  `}
+`;
 
 export default function Menu() {
-    const [active, setActive] = useState(tabs[0]);
-    
-    const burgerMenu = burgers.map((data, key) => {
-        return (
-          <div className="Menu-items" key={key}>
-            {data.title + "," + data.description + "," + data.price + "," + data.image}
-          </div>
-        );
-      })
-    
-    const drinksMenu = drinks.map((data, key) => {
-        return (
-          <div className="Menu-items" key={key}>
-            {data.title + "," + data.description + "," + data.price + "," + data.image}
-          </div>
-        );
-      })
+  const [active, setActive] = useState(tabs[0]);
 
-      const sidesMenu = sides.map((data, key) => {
-        return (
-          <div className="Menu-items" key={key}>
-            {data.title + "," + data.description + "," + data.price + "," + data.image}
-          </div>
-        );
-      })
-    const renderMenu = () => {
-        if (active === "Burgers") {
-          return burgerMenu;
-        } else if (active === "Drinks") {
-          return drinksMenu;
-        } else {
-            return sidesMenu;
+  const burgerMenu = burgers.map((data, key) => {
+    return (
+      <div className="Menu-items" key={key}>
+       <div>{ data.image }</div> 
+       <div>{data.title}</div>
+       <div>{data.price}</div>
+      </div>
+    );
+  });
 
-          }
-      }
- 
+  const drinksMenu = drinks.map((data, key) => {
+    return (
+      <div className="Menu-items" key={key}>
+       <div>{ data.image }</div> 
+       <div>{data.title}</div>
+       <div>{data.price}</div>
+      </div>
+    );
+  });
+
+  const sidesMenu = sides.map((data, key) => {
+    return (
+      <div className="Menu-items" key={key}>
+       <div>{ data.image }</div> 
+       <div>{data.title}</div>
+       <div>{data.price}</div>
+      </div>
+    );
+  });
+  const renderMenu = () => {
+    if (active === "Burgers") {
+      return burgerMenu;
+    } else if (active === "Drinks") {
+      return drinksMenu;
+    } else {
+      return sidesMenu;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-          {tabs.map(tab  => (
-              <div
-              key={tab}
-              active={active === tab}
-              onClick={() => setActive(tab)}
-              >
-                  {tab}
-              </div>
-
-          ))}
-        {renderMenu()}  
-      </header>
+    <div className="App-Menu">
+      <div className="Hole-Menu">
+        <h1 className="H1-Menu"> Menu </h1>
+        <div className="hole-tab">
+        <div className="Hole-Menu">
+          
+      <div className="Tab-Menu">
+        {tabs.map((tab) => (
+          <Tab key={tab} active={active === tab} onClick={() => setActive(tab)}>
+            {tab}
+          </Tab>
+        ))}
+        </div>
+        {renderMenu()}
+        </div>
+        </div>
+        </div>
     </div>
   );
 }
