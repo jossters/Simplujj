a699f59f6e2d8c094535ef1853619c949611f416
Johnny Murillo <jossters@gmail.com>
2021-12-31 19:58:53 -0800

    added google maps to location

--- client/src/components/Locations.js
+++ client/src/components/Locations.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
+import GoogleMapReact from "google-map-react";
 import "../App.css";
 
 export default function Location() {
-    return(
-        <div className="App">
-            <header className="App-header">
-            <h1>Location</h1>
-            </header>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Location</h1>
+        <div
+          className="col text-center"
+          style={{ width: "85%", height: "70vh" }}
+        >
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: "" }}
+            defaultZoom={17}
+            defaultCenter={{
+              lat: 34.107041,
+              lng: -117.922038,
+            }}
+            >
+            </GoogleMapReact>
+          
         </div>
-
-    );
-    }
+      </header>
+    </div>
+  );
+}
